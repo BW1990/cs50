42b37aa905c7ed6e157dc1fe69326d745d536e56
Brian <brianwinward@gmail.com>
2017-05-16 20:21:12 -0400

    git ignore and week 1 lecture examples

---
+++ week1/examples/overflowFor.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  int n = 0;
+  for (int i = 0; i < 64; i++) {
+    printf("n is %i\n", n);
+    n *= 2;
+  }
+}
---
+++ week1/examples/sizeof.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  printf("bool is %lu", sizeof(bool));
+  printf("char is %lu", sizeof(char));
+  printf("double is %lu", sizeof(double));
+  printf("float is %lu", sizeof(float));
+  printf("int is %lu", sizeof(int));
+  printf("long long is %lu", sizeof(long long));
+  printf("string is %lu", sizeof(string));
+}
