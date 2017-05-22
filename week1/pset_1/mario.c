8a85af59667a7026ca2d6ba7555dfb23e824128b
Brian <brianwinward@gmail.com>
2017-05-22 19:19:53 -0400

    fixed tabbing on mario.c

--- week1/pset_1/mario.c
+++ week1/pset_1/mario.c
@@ -2,16 +2,16 @@
 #include <cs50.h>
 
 int main(void) {
-    printf("Height: ");
-    int height = get_int();
+  printf("Height: ");
+  int height = get_int();
 
-    for (int i = 0; i < height; i++) {
-        for (int tab = (height - i); tab > 0; tab--) {
-            printf(" ");
-        }
-        for (int j = 0; j < i; j++) {
-            printf("#");
-        }
-        printf("#\n");
+  for (int i = 0; i < height; i++) {
+    for (int tab = (height - i); tab > 0; tab--) {
+      printf(" ");
     }
+    for (int j = 0; j < i; j++) {
+      printf("#");
+    }
+    printf("#\n");
+  }
 }
